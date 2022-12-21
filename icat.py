f73001e6a5afde21dab71b81e0ef5d59150589ed
Mark Valentino <mike9377mike9377@gmail.com>
2022-12-21 17:32:02 -0500

    added sharpen functionality

--- icat.py
+++ icat.py
@@ -1,9 +1,10 @@
-from PIL import Image, ImageOps
+from PIL import Image, ImageOps, ImageFilter
 from numpy import asarray
 import cv2 as cv2
 import sys
 
 def printImage():
+    data = asarray(bw_img)
     for i in range(data.shape[0]):
         # Two chars are printed per pixel so that the image's aspect ratio is preserved.
         for j in range(data.shape[1]):
@@ -17,22 +18,21 @@ def printImage():
                 print("░░", end="")
             else:
                 print("  ", end="")
-            
         print()
 
 
 if (len(sys.argv) == 1):
     print("USAGE: enter file path to an image.")
-elif (sys.argv[2] == "0-i"):
-    image = Image.open(sys.argv[1])
-    # Resize image to maintian aspect ratio as well as convert it to black and white.
-    bw_img  = ImageOps.contain(image, (40,40)).convert("L")
-    bw_img = ImageOps.invert(bw_img)
-    data = asarray(bw_img)
-    printImage()
 else:
     image = Image.open(sys.argv[1])
     # Resize image to maintian aspect ratio as well as convert it to black and white.
     bw_img  = ImageOps.contain(image, (40,40)).convert("L")
-    data = asarray(bw_img)
-    printImage()
+    
+    if (sys.argv[2] == "0-i"):
+        bw_img = ImageOps.invert(bw_img)
+        printImage()
+    elif (sys.argv[2] == "0-s"):
+        bw_img = bw_img.filter(ImageFilter.SHARPEN)
+        printImage()
+    else:
+        printImage()
