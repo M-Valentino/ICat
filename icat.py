2f2ad28f7cc3c224738feff1ca3f63ca36baf248
Mark Valentino <79779618+M-Valentino@users.noreply.github.com>
2022-12-21 17:02:27 -0500

    Adding image inverstion capability

--- icat.py
+++ icat.py
@@ -1,16 +1,9 @@
-
 from PIL import Image, ImageOps
 from numpy import asarray
 import cv2 as cv2
 import sys
 
-
-def readAndPrint():
-    image = Image.open(sys.argv[1])
-    # Resize image to maintian aspect ratio as well as convert it to black and white.
-    bw_img  = ImageOps.contain(image, (40,40)).convert("L")
-    data = asarray(bw_img)
-
+def printImage():
     for i in range(data.shape[0]):
         # Two chars are printed per pixel so that the image's aspect ratio is preserved.
         for j in range(data.shape[1]):
@@ -30,6 +23,16 @@ def readAndPrint():
 
 if (len(sys.argv) == 1):
     print("USAGE: enter file path to an image.")
+elif (sys.argv[2] == "0-i"):
+    image = Image.open(sys.argv[1])
+    # Resize image to maintian aspect ratio as well as convert it to black and white.
+    bw_img  = ImageOps.contain(image, (40,40)).convert("L")
+    bw_img = ImageOps.invert(bw_img)
+    data = asarray(bw_img)
+    printImage()
 else:
-    readAndPrint()
-
+    image = Image.open(sys.argv[1])
+    # Resize image to maintian aspect ratio as well as convert it to black and white.
+    bw_img  = ImageOps.contain(image, (40,40)).convert("L")
+    data = asarray(bw_img)
+    printImage()
