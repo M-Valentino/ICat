af9754e4fb896202c19ac482a8529ccf24818a78
Mark Valentino <79779618+M-Valentino@users.noreply.github.com>
2022-12-20 21:56:34 -0500

    Add files via upload

---
+++ icat.py
@@ -0,0 +1,35 @@
+
+from PIL import Image, ImageOps
+from numpy import asarray
+import cv2 as cv2
+import sys
+
+
+def readAndPrint():
+    image = Image.open(sys.argv[1])
+    # Resize image to maintian aspect ratio as well as convert it to black and white.
+    bw_img  = ImageOps.contain(image, (40,40)).convert("L")
+    data = asarray(bw_img)
+
+    for i in range(data.shape[0]):
+        # Two chars are printed per pixel so that the image's aspect ratio is preserved.
+        for j in range(data.shape[1]):
+            if (data[i][j] >= 204):
+                print("██", end="")
+            elif (data[i][j] < 204 and data[i][j] >= 153):
+                print("▓▓", end="")
+            elif (data[i][j] < 153 and data[i][j] >= 102):
+                print("▒▒",end="")
+            elif (data[i][j] < 102 and data[i][j] >= 51):
+                print("░░", end="")
+            else:
+                print("  ", end="")
+            
+        print()
+
+
+if (len(sys.argv) == 1):
+    print("USAGE: enter file path to an image.")
+else:
+    readAndPrint()
+
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo cp icat.py /usr/local/bin/ 
+sudo cp icat /usr/local/bin/
+
+export PATH=$PATH:/usr/local/bin/icat
