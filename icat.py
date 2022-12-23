53ac1208bada7da17e204a8a2b3fd0631520c008
Mark Valentino <mike9377mike9377@gmail.com>
2022-12-23 13:47:29 -0500

    fixed bug where usage is not said when no arg is supplied. Now ICat can support 3 args

--- icat.py
+++ icat.py
@@ -20,18 +20,14 @@ def printImage():
         print()
 
 
-if (len(sys.argv) == 1):
-    print("USAGE: enter file path to an image.")
-else:
-    image = Image.open(sys.argv[1])
-    # Resize image to maintian aspect ratio as well as convert it to black and white.
-    bw_img  = ImageOps.contain(image, (40,40)).convert("L")
+image = Image.open(sys.argv[1])
+# Resize image to maintian aspect ratio as well as convert it to black and white.
+bw_img  = ImageOps.contain(image, (40,40)).convert("L")
     
-    if (sys.argv[2] == "0-i"):
-        bw_img = ImageOps.invert(bw_img)
-        printImage()
-    elif (sys.argv[2] == "0-s"):
-        bw_img = bw_img.filter(ImageFilter.SHARPEN)
-        printImage()
-    else:
-        printImage()
+if (sys.argv[2] == "0-i" or sys.argv[3] == "0-i"):
+    bw_img = ImageOps.invert(bw_img)
+
+if (sys.argv[2] == "0-s" or sys.argv[3] == "0-s"):
+    bw_img = bw_img.filter(ImageFilter.SHARPEN)
+
+printImage()
