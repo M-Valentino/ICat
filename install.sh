9d1c3150e66bea4872b0c70881c6e45c43e5bfc0
Mark Valentino <mike9377mike9377@gmail.com>
2022-12-24 18:07:59 -0500

    Made installation more verbose when running

--- install.sh
+++ install.sh
@@ -2,4 +2,19 @@
 sudo cp icat.py /usr/local/bin/ 
 sudo cp icat /usr/local/bin/
 
+FILE=/usr/local/bin/icat.py
+if test -f "$FILE"; then
+    echo "✅\t$FILE installed."
+else
+    echo "❌\t$FILE could not be installed."
+fi
+FILE=/usr/local/bin/icat
+if test -f "$FILE"; then
+    echo "✅\t$FILE installed."
+else
+    echo "❌\t$FILE could not be installed."
+fi
+
+echo "ℹ️\tMake sure the required Python packages are installed. They are listed in README.md"
+
 export PATH=$PATH:/usr/local/bin/icat
