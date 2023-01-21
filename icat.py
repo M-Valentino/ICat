50f9c23f081d4ed7492c6f9954aefd87357a114a
Mark Valentino <mike9377mike9377@gmail.com>
2023-01-20 21:05:50 -0500

    added ability to print SVG files

--- icat.py
+++ icat.py
@@ -2,6 +2,7 @@ from PIL import Image, ImageOps, ImageFilter
 from numpy import asarray
 import sys
 import pathlib
+import cairosvg
 
 """
 This file contains the core functionality of ICat. This file is exectuted by a bash file
@@ -48,47 +49,49 @@ def printImageLightBG():
         print()
 
 if pathlib.Path(sys.argv[1]).suffix == ".svg":
-    print("SVG not yet supported.")
+    cairosvg.svg2png(url=sys.argv[1], write_to=("icat-output.png"))
+    image = Image.open("icat-output.png")
 else:
     image = Image.open(sys.argv[1])
-    """
-    icat_settings.cfg will determine whether images are printed in full width or half width
-    pixels. Half width pixels have two times as much horizontal detail as full width ones.
-    icat_settings.cfg will also determine the right function to use when printing to terminal
-    with a light or dark background. 
-    """
-    settings = open("/usr/local/bin/icat_settings.cfg", "r")
-    if "half" in settings.readline():
-        width, height = image.size
-        # Resizes image to be 50% as wide since image will be printed in half width "pixels".
-        image = image.resize((width, round(height / 2)), Image.LANCZOS)
-        # Is a multiplier used in doubling horizontal detail for half width pixels.
-        scaleX = 2
-    else:
-        # Is a multiplier used in keeping horizontal detail the same (for full width pixels). 
-        scaleX = 1
 
-    options = sys.argv[2:]
-    if "0x1.5" in options:
-        # .convert("L") converts image to black and white. 
-        image = ImageOps.contain(image, (60 * scaleX, 60)).convert("L")
-    elif "0x2" in options:
-        image = ImageOps.contain(image, (80 * scaleX, 80)).convert("L")
-    else:
-        # Default size printed if user gives no args.
-        image  = ImageOps.contain(image, (40 * scaleX, 40)).convert("L")
-    
-    if "0-s" in options:
-        image = image.filter(ImageFilter.SHARPEN)
-    if "0-c" in options:
-        image = image.filter(ImageFilter.CONTOUR)
-    if "0-i" in options:
-        image = ImageOps.invert(image)
+"""
+icat_settings.cfg will determine whether images are printed in full width or half width
+pixels. Half width pixels have two times as much horizontal detail as full width ones.
+icat_settings.cfg will also determine the right function to use when printing to terminal
+with a light or dark background. 
+"""
+settings = open("/usr/local/bin/icat_settings.cfg", "r")
+if "half" in settings.readline():
+    width, height = image.size
+    # Resizes image to be 50% as wide since image will be printed in half width "pixels".
+    image = image.resize((width, round(height / 2)), Image.LANCZOS)
+    # Is a multiplier used in doubling horizontal detail for half width pixels.
+    scaleX = 2
+else:
+    # Is a multiplier used in keeping horizontal detail the same (for full width pixels). 
+    scaleX = 1
 
-    if "light" in settings.readline():
-        printImageLightBG()
-    else:
-        printImageDarkBG()
-    
-    image.close()
-    settings.close()
+options = sys.argv[2:]
+if "0x1.5" in options:
+    # .convert("L") converts image to black and white. 
+    image = ImageOps.contain(image, (60 * scaleX, 60)).convert("L")
+elif "0x2" in options:
+    image = ImageOps.contain(image, (80 * scaleX, 80)).convert("L")
+else:
+    # Default size printed if user gives no args.
+    image  = ImageOps.contain(image, (40 * scaleX, 40)).convert("L")
+
+if "0-s" in options:
+    image = image.filter(ImageFilter.SHARPEN)
+if "0-c" in options:
+    image = image.filter(ImageFilter.CONTOUR)
+if "0-i" in options:
+    image = ImageOps.invert(image)
+
+if "light" in settings.readline():
+    printImageLightBG()
+else:
+    printImageDarkBG()
+
+image.close()
+settings.close()
